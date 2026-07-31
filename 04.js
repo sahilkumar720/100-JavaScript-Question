a00cf7c7ee8760c6a8dfa73fb4228460514cd758
Sahil Kumar <sahilkumar7369@gmail.com>
2026-07-31 23:24:57 +0530

    04 question complete

---
+++ 04.js
@@ -0,0 +1,18 @@
+// question no:4
+// find largest number in an array
+
+ 
+function largestElement(arr) {
+    let largestNum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largestNum) {
+            largestNum = arr[i];
+        }
+    }
+    return largestNum;
+}
+
+const num1 = [10, 15, 18, 20, 23];
+const result = largestElement(num1);
+
+console.log("The largest element in the array is:" + result);
